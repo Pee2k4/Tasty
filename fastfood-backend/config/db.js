577b7035e4188849802ec3897cd75cdbf9a440d2
phong <phongke2k4@gmail.com>
2026-04-29 17:16:23 +0700

    deeploy

--- fastfood-backend/config/db.js
+++ fastfood-backend/config/db.js
@@ -6,11 +6,16 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306,
+    port: process.env.DB_PORT || 22839, // Aiven thường dùng 22839
 
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    
+    // THÊM DÒNG NÀY ĐỂ CHẠY ĐƯỢC TRÊN CLOUD
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool.promise();
